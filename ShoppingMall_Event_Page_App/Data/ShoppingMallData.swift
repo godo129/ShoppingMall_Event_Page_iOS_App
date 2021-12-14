261fb01f98f101936ad83c823df888d1fc5343a8
godo <godo129@naver.com>
2021-12-14 23:52:01 +0900

    구조 만들기

---
+++ ShoppingMall_Event_Page_App/Data/ShoppingMallData.swift
@@ -0,0 +1,21 @@
+//
+//  ShoppingMallData.swift
+//  ShoppingMall_Event_Page_App
+//
+//  Created by hong on 2021/12/14.
+//
+
+import UIKit
+
+
+struct ShoppingMallList {
+    
+    var ShoppingMallName: String
+    
+}
+
+struct eventDatum {
+    var name: String
+    var imageURL: [String] = []
+    var eventURL: [String] = []
+}
